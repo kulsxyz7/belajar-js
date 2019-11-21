242be8f10fe848578b69554cbb265471535afaf0
kulsxyz7 <kulsxyz7@gmail.com>
2019-11-21 13:56:44 +0700

    mengubah isi file dalam script.js

--- vlmkubus-js/script.js
+++ vlmkubus-js/script.js
@@ -1,12 +1,14 @@
-var a = 8;
-var b = 3;
-var volumeA;
-var volumeB;
-var total;
+function jumlahVolumeDuaKubus(a, b) {
+    var volumeA;
+    var volumeB;
+    var total;
 
-volumeA = a * a * a;
-volumeB = b * b * b;
+    volumeA = a * a * a;
+    volumeB = b * b * b;
 
-total = volumeA + volumeB;
+    total = volumeA + volumeB;
 
-console.log(total);
+    return total;
+}
+
+alert(jumlahVolumeDuaKubus(8, 3));
